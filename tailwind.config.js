4c8d7ca4f6ab4117025b32d5d04832cc439d1154
julians22 <dabnerjulian@yahoo.com>
2021-03-18 15:03:24 +0700

    fix purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     purge: [
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
+        "./resources/**/*.css",
         "./resources/**/*.vue"
     ],
     darkMode: false, // or 'media' or 'class'
