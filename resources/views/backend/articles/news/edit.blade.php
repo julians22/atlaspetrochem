a4c11bbbd61b65de99c5515847b076bff3bcfef4
Julian <dabnerjulian@yahoo.com>
2021-04-19 15:18:12 +0700

    add table

--- resources/views/backend/articles/news/edit.blade.php
+++ resources/views/backend/articles/news/edit.blade.php
@@ -122,7 +122,7 @@
                     toolbar: [
                         ['style', ['bold', 'italic', 'underline', 'clear']],
                         ['font', ['strikethrough', 'superscript', 'subscript']],
-                        ['link'],
+                        ['insert', ['link','table']],
                         ['fontsize', ['fontsize']],
                         ['color', ['color']],
                         ['para', ['ul', 'ol', 'paragraph']],
